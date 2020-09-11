a2240011cd9f032c9c63663714769a4676b266fd
NatanTavares <nattantavares.s15@gmail.com>
2020-09-10 22:14:03 -0300

    a3: Preparing methods for the car class

--- src/interfaces/ICarroService.ts
+++ src/interfaces/ICarroService.ts
@@ -1,3 +1,12 @@
+import { Carro } from 'src/models/Carro';
+import { Observable } from 'rxjs';
+import { CarroService } from 'src/services/CarroService';
+
 export interface ICarroService {
-    
+    cadastrar(carro: Carro) : Observable<Carro>;
+    remover(carro_id: number) : boolean;
+    editar(carro: Carro) : Observable<Carro>;
+    listar() : Observable<Carro[]>;
+    buscar(carro_id: number) : Observable<CarroService>;
+    calcularLitros(carro: Carro, tipoCombustivel: string, distancia: number) : Number;
 }
--- src/models/Carro.ts
+++ src/models/Carro.ts
@@ -1,3 +1,8 @@
 export class Carro {
-    
+    public id: number = null;
+    public modelo: string = null;
+    public montadora: string = null;
+    public consumoAlcool: number = null;
+    public consumoGasolina: number = null;
+    public usuarioID: number = null;
 }
