7c9b3b1613e230a48dc76bdad30c8e2bea1789f7
NatanTavares <nattantavares.s15@gmail.com>
2020-09-10 22:33:36 -0300

    a3: Basic validation of inputs for user registration

--- src/services/UsuarioService.ts
+++ src/services/UsuarioService.ts
@@ -10,7 +10,12 @@ export class UsuarioService implements IUsuarioService {
     constructor (private _httpClient: HttpClient) {}
 
     cadastrar(usuario: Usuario): Observable<Usuario> {
-        throw new Error("Method not implemented.");
+        if (!usuario.nome) throw new Error("O campo Nome é obrigatório!");
+        if (!usuario.email) throw new Error("O campo Email é obrigatório!");
+        if (!usuario.senha) throw new Error("O campo Senha é obrigatório!");
+        if (usuario.confirmarSenha != usuario.senha) throw new Error("Senhas não coincidem!");
+
+        throw new Error("You can to salve.");
     }
     atualizar(usuario: Usuario): Observable<Usuario> {
         throw new Error("Method not implemented.");
