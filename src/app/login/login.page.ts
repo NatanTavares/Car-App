e283f48de46a5a6869bb49547f87d1452f707e12
Natan <nattantavares.s15@gmail.com>
2020-09-17 19:26:41 -0300

    a4: Menu disabled

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _menu: MenuController) {
+    this._menu.swipeGesture(false);
   }
 
+  ngOnInit() {
+
+  }
 }
