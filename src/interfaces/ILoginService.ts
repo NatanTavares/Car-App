6183518dd4162aa8669cf57ffe88e254a770b60d
Natan <nattantavares.s15@gmail.com>
2020-09-17 22:26:32 -0300

    a4: Feature (login)

--- src/interfaces/ILoginService.ts
+++ src/interfaces/ILoginService.ts
@@ -1,3 +1,7 @@
+import { Observable } from 'rxjs';
+import { Login } from 'src/models/Login';
+import { Usuario } from 'src/models/Usuario';
 export interface ILoginService {
-    
+  login(dadosUsuario: Login) : Observable<Usuario>;
+  
 }
--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,5 +1,14 @@
+import { Observable } from 'rxjs';
 import { ILoginService } from 'src/interfaces/ILoginService';
+import { Login } from 'src/models/Login';
+import { Usuario } from 'src/models/Usuario';
 
 export class LoginService implements ILoginService {
+  login(dadosUsuario: Login): Observable<Usuario> {
+    if (!dadosUsuario.email)  throw new Error('O Email é obrigatorio.');
+    if (!dadosUsuario.senha)  throw new Error('A Senha é obrigatorio.');
+
+    throw new Error('O login poderá ser efetuado.');
+  }
 
 }
