0203f271f90286e7ed530e49a087d7d8f3db3c4f
Natan <nattantavares.s15@gmail.com>
2020-09-17 22:30:07 -0300

    a4: Implemented functionality (login)

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { Login } from 'src/models/Login';
+import { LoginService } from 'src/services/LoginService';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +10,33 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public login: Login = new Login;
 
-  constructor(private _menu: MenuController) {
-    this._menu.swipeGesture(false);
+  constructor(private _menu: MenuController, 
+    private _router: Router, 
+    private _loginService: LoginService) {
+      
+  }
+
+  onLogin() {
+    console.log('> This.login:', this.login);
+    this._loginService.login(this.login);
+  }
+
+  onCadastro() {
+    console.log('> This._route:', this._router);
+    this._router.navigate(['/cadastrar-usuario']);
   }
 
   ngOnInit() {
+    // this._menu.swipeGesture(false);
+  }
 
+  ionViewDidEnter() {
+    this._menu.swipeGesture(false);
+  }
+
+  inoViewWillLeave() {
+    this._menu.swipeGesture(true);
   }
 }
