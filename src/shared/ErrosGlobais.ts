82cec6e66ac6231e592fb90b2eebd455a282830f
NatanTavares <nattantavares.s15@gmail.com>
2020-09-24 19:24:38 -0300

    Constumizad alert

--- src/shared/ErrosGlobais.ts
+++ src/shared/ErrosGlobais.ts
@@ -1,9 +1,31 @@
-import { ErrorHandler } from '@angular/core';
+import { ErrorHandler, Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  'providedIn': 'root'
+})
 
 export class ErrosGlobais implements ErrorHandler {
+
+  constructor(private _alert: AlertController) {
+
+  }
+
   handleError(error: any): void {
     // throw new Error('Method not implemented.');
-    console.log(error instanceof Error);
+    console.log(error);
+  }
+
+  async presentAlert() {
+    const alert = await this._alert.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
