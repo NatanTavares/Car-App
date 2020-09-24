f543dba106232b5b3ac769fcfb5b79f75dad9e74
NatanTavares <nattantavares.s15@gmail.com>
2020-09-24 20:29:13 -0300

    a5: Url ready to consume api

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -2,8 +2,11 @@ import { Observable } from 'rxjs';
 import { ILoginService } from 'src/interfaces/ILoginService';
 import { Login } from 'src/models/Login';
 import { Usuario } from 'src/models/Usuario';
+import { Global } from 'src/shared/Global';
 
 export class LoginService implements ILoginService {
+  public apiUrl: string = Global.ApiUrl + "usuarios/login";
+
   login(dadosUsuario: Login): Observable<Usuario> {
     if (!dadosUsuario.email)  throw new Error('O Email é obrigatorio.');
     if (!dadosUsuario.senha)  throw new Error('A Senha é obrigatorio.');
